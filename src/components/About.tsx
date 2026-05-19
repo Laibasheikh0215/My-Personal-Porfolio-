58ebdd162951c08f986e5a066db7a7797e6bfe4d
Laiba Sheikh <slaiba25@gmail.com>
2026-05-19 17:28:18 +0500

    fixing bugs

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,7 +5,7 @@ const About = () => {
     <div className="about-section" id="about">
       <div className="about-me">
         <h3 className="title">About Me</h3>
-        <p className="para">
+        <p className="para" style={{ fontSize: '20px' }}>
           I'm Laiba Sheikh, a Junior Full Stack Developer from Karachi with a degree in Software Engineering and a diploma in Modern Web Application Development. I specialize in the PERN/MERN stack, building responsive web apps and RESTful APIs that are clean, scalable, and user-friendly.
           From real-time restaurant systems to weather dashboards, I love turning ideas into fully functional products. I'm currently looking for a junior developer role where I can contribute, learn, and grow.
           Let's build something together.
